e5114fe73fbc9607adbcfca5646b0331b08293ce
RodrigoCarrizo94 <87373321+RodrigoCarrizo94@users.noreply.github.com>
2021-09-14 21:07:08 -0300

    Revisar

--- src/turismo/Sistema.java
+++ src/turismo/Sistema.java
@@ -1,9 +1,39 @@
 package turismo;
 
+import java.io.*;
+
 public class Sistema {
 
 	public static void main(String[] args) {
+		FileReader fr = null;
+		BufferedReader br = null;
+		try {
+			fr = new FileReader("posiciones.in");
+			br = new BufferedReader(fr);
+			String linea;
+			while ((linea = br.readLine()) != null) {
+				try {
+					String[] datos = linea.split(",");
+					String nombre = datos[0];
+					int costo = Integer.parseInt(datos[1]);
+					double tiempo = Double.parseDouble(datos[2]);
+					TipoDeAtraccion tipo = datos[3]; // Corregir
+					Usuario user = new Usuario(nombre, costo, tiempo, tipo);
+				} catch (NoValidoException n) {
+					System.err.println("Uno de los valores introducidos no es correcto.");
+				}
 
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fr != null) {
+					fr.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
 	}
-
 }
